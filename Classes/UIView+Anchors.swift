2a4e1f8629c3755e662e3b9a6bae14d403d5b7a7
Itthiphon Wiwatthanasathit <oldster189@gmail.com>
2019-10-03 17:28:17 +0700

    .

---
+++ Classes/UIView+Anchors.swift
@@ -0,0 +1,9 @@
+//
+//  UIView+Anchors.swift
+//  PageMenuFramework
+//
+//  Created by Codemobiles Old'ster on 3/10/2562 BE.
+//  Copyright © 2562 PageMenu. All rights reserved.
+//
+
+import Foundation
