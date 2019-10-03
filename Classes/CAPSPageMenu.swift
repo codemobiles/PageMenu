cae415e1dbe18f162b7922035325ef7a825b1e51
CodeMobiles <chaiyasit.t@gmail.com>
2019-10-03 11:48:32 +0700

    Update CAPSPageMenu.swift
    Change icon tabs

--- Classes/CAPSPageMenu.swift
+++ Classes/CAPSPageMenu.swift
@@ -1160,8 +1160,8 @@ open class CAPSPageMenu: UIViewController, UIScrollViewDelegate, UIGestureRecogn
     
     /****************************** Meaw ******************************/
     func addImageOverMyMenuItem(index: Int) {
-        let imageWidth = CGFloat(36.0)
-        let imageHeight = CGFloat(36.0)
+        let imageWidth = CGFloat(50.0)
+        let imageHeight = CGFloat(50.0)
         
         //        var labelFrame = self.menuItems[index].titleLabel!.frame
         //        labelFrame.origin.x = imageWidth / 1.5
